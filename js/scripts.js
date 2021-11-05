0e43e8d2c49a255c4d83bf215cdfc08408bbbb91
Ethan Bear <ehwbear@icloud.com>
2021-11-05 09:59:38 -0700

    Create function to return pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("#submitForm").click(function(event) {
-function myPizza([toppings], size) {
-  this.toppings = toppings;
-  this.size = size;
-}
+    function myPizza([toppings], size) {
+      this.toppings = toppings;
+      this.size = size;
+    }
+    
+    Contact.prototype.fullName = function() {
+      return "You've ordered a " + this.size + " pizza with " + this.toppings;
+    };
