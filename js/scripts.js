6260a77a1cccf9a64ddbbda932b8acd331a9d997
Ethan Bear <ehwbear@icloud.com>
2021-11-09 14:25:47 -0800

    Finalize project

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,74 @@
 //BUSINESS LOGIC
 
-$(document).ready(function() {
-  $("#submitForm").click(function(event) {
-    function pizza([toppings], size, delivery) {
-      this.pizzaToppings = toppings;
-      this.pizzaSize = size;
-      this.deliveryMethod = delivery;
-      this.price = price;
-    }
-  });
+
+function Pizza(toppings, size, delivery) {
+  this.pizzaToppings = toppings;
+  this.pizzaSize = size;
+  this.deliveryMethod = delivery;
+  this.price = 10
+}
 
 //Pizza Prices
-pizza.prototype.price = function() {
-  if (this.pizzaToppings === "Anchovies") {
-    this.price += 2;
-  } else if (this.pizzaToppings === "Krabby Patty secret formula (discontinued)"){
-    this.price += 3;
-  } else (this.pizzaToppings === "Coral")
-    this.price += 1;
-  }
+Pizza.prototype.setPrice = function() {
 
-  if (this.pizzaSize === "Medium") {
-    this.price += 0;
-  } else if (this.pizzaSize === "Large") {
-    this.price += 4
-  } else (this.pizzaSize === "WUMBO")
-    this.price += 250
+  let toppingCost = 3
+  this.price += this.pizzaToppings.length * toppingCost;
 
-  if (this.deliveryMethod === "byFoot") {
-    this.price -= 3;
-  } else if (this.deliveryMethod === "Boat") {
-    this.price +=5;
-  } else (this.deliveryMethod === "A Rock")
-    this.price +=200;
-      return "Your " + this.pizzaSize + " Krusty Krab pizza with " + this.pizzaToppings + "will arrive soon via " + this.deliveryMethod + "! Thanks for your order, and more importantly: Thanks for your money! Arr-ar-ar-ar-ar-ar! Your total is " + this.price;
-  });
+if (this.pizzaSize === "Medium") {
+  this.price += 0;
+} else if (this.pizzaSize === "Large") {
+  this.price += 4
+} else if (this.pizzaSize === "WUMBO") {
+  this.price += 250
+}
 
+if (this.deliveryMethod === "byFoot") {
+  this.price -= 3;
+} else if (this.deliveryMethod === "Boat") {
+  this.price +=5;
+} else if (this.deliveryMethod === "A Rock") {
+  this.price +=200;
+}}
+
+Pizza.prototype.getMessage = function() {
+  return "Your " + this.pizzaSize + " Krusty Krab pizza with " + this.pizzaToppings.join(", ") + " will arrive soon via " + this.deliveryMethod + "! Thanks for your order, and more importantly: Thanks for your money! Arr-ar-ar-ar-ar-ar! Your total is $ " + this.price;
+}
+
+$(document).ready(function() {
+  $(".submitForm").click(function(event) {
+    // get values
+
+    let toppings = []
+    if ($("#coral").is(":checked")) {
+      toppings.push("coral")
+    }
+    if ($("#anchovies").is(":checked")) {
+      toppings.push("anchovies")
+    }
+    if ($("#formula").is(":checked")) {
+      toppings.push("the Krabby Patty secret formula")
+    }
+
+    let size = $("input[name=size]:checked").val()
+
+    let delivery = $("input[name=delivery]:checked").val()
+
+
+
+    // topping array, size, delivery
+        // create our pizza
+
+
+    let myPissa = new Pizza(toppings, size, delivery)
+    // set price
+    myPissa.setPrice()
+    // get message
+    let msg = myPissa.getMessage()
+    // update UI
+    $("#order").append(msg)
+
+
+
+    });
+});
 // USER-INTERFACE LOGIC
